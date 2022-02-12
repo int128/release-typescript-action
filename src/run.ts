52568994f6affb6fdfbc558b7e1ee273f8d829ae
Hidetake Iwata <int128@gmail.com>
2022-02-12 21:10:31 +0900

    Fix release note and don't release if no change (#6)

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as github from '@actions/github'
-import { findLatestMinor } from './semver'
+import { computeNextTag } from './semver'
 
 type Inputs = {
   majorVersion: number
@@ -48,60 +48,68 @@ const updateCurrentTag = async (inputs: Inputs) => {
   await exec.exec('git', ['push', 'origin', '-f', currentTag, majorTag])
 }
 
+const gitStatus = async (): Promise<string> => {
+  const chunks: Buffer[] = []
+  await exec.exec('git', ['status', '--porcelain'], { listeners: { stdout: (b) => chunks.push(b) } })
+  return Buffer.concat(chunks).toString().trim()
+}
+
 const createNextMinorRelease = async (inputs: Inputs) => {
-  const tags = await getTags()
-  core.info(`Found ${tags.length} tags`)
-  const latestMinor = findLatestMinor(tags, inputs.majorVersion)
-  core.info(`Latest minor is ${latestMinor}`)
+  const octokit = github.getOctokit(inputs.token)
+
   const majorTag = `v${inputs.majorVersion}`
   core.info(`Major tag is ${majorTag}`)
-  const nextTag = `v${inputs.majorVersion}.${latestMinor + 1}.0`
+  const currentTag = await findCurrentTag(majorTag)
+  core.info(`Current tag is ${currentTag ?? 'not found'}`)
+  const nextTag = computeNextTag(currentTag, majorTag)
   core.info(`Next tag is ${nextTag}`)
 
   await exec.exec('sed', ['-i', '-e', 's|^/dist.*||g', '.gitignore'])
   await exec.exec('git', ['add', '.gitignore'])
   await exec.exec('git', ['add', 'dist'])
-  if ((await gitStatus()) === '') {
-    core.info(`Nothing to commit`)
-    return
-  }
-
   await exec.exec('git', ['config', 'user.name', 'github-actions'])
   await exec.exec('git', ['config', 'user.email', 'github-actions@github.com'])
   await exec.exec('git', ['commit', '-m', `Release ${nextTag}`])
+  await exec.exec('git', ['tag', nextTag])
+  await exec.exec('git', ['tag', '-f', majorTag])
+
+  if (currentTag !== undefined) {
+    const code = await exec.exec('git', ['diff', '--exit-code', currentTag, nextTag, '--', 'dist'], {
+      ignoreReturnCode: true,
+    })
+    if (code === 0) {
+      core.info('Nothing to release')
+      return
+    }
+  }
   if (inputs.dryRun) {
     core.info(`Exit due to dry-run`)
     return
   }
-
-  await exec.exec('git', ['tag', nextTag])
-  await exec.exec('git', ['tag', '-f', majorTag])
   await exec.exec('git', ['push', 'origin', '-f', nextTag, majorTag])
 
-  const octokit = github.getOctokit(inputs.token)
   core.info(`Creating a release for tag ${nextTag}`)
-  const { data: release } = await octokit.rest.repos.createRelease({
+  const { data: releaseNote } = await octokit.rest.repos.generateReleaseNotes({
     ...github.context.repo,
     tag_name: nextTag,
-    name: nextTag,
-    generate_release_notes: true,
+    previous_tag_name: currentTag,
+  })
+  const { data: release } = await octokit.rest.repos.createRelease({
+    ...github.context.repo,
+    ...releaseNote,
+    tag_name: nextTag,
   })
   core.info(`Created a release as ${release.html_url}`)
 }
 
-const getTags = async () => {
+const findCurrentTag = async (majorTag: string) => {
   const tags: string[] = []
   await exec.exec('git', ['fetch', '--tags', '--prune-tags', '--prune'])
-  await exec.exec('git', ['tag'], {
+  await exec.exec('git', ['tag', '--list', '--contains', majorTag], {
     listeners: {
       stdline: (l) => tags.push(l.trim()),
     },
+    ignoreReturnCode: true,
   })
-  return tags
-}
-
-const gitStatus = async (): Promise<string> => {
-  const chunks: Buffer[] = []
-  await exec.exec('git', ['status', '--porcelain'], { listeners: { stdout: (b) => chunks.push(b) } })
-  return Buffer.concat(chunks).toString().trim()
+  return tags.filter((tag) => tag != majorTag).pop()
 }
--- tests/semver.test.ts
+++ tests/semver.test.ts
@@ -1,26 +1,13 @@
-import { findLatestMinor } from '../src/semver'
+import { computeNextTag } from '../src/semver'
 
 test('a tag is given', () => {
-  expect(findLatestMinor(['v1.1.0'], 1)).toBe(1)
+  expect(computeNextTag('v1.1.0', 'v1')).toBe('v1.2.0')
 })
 
-test('tags are given', () => {
-  expect(findLatestMinor(['v1.0.0', 'v1.1.0', 'v1.2.0'], 1)).toBe(2)
+test('undefined is given', () => {
+  expect(computeNextTag(undefined, 'v1')).toBe('v1.1.0')
 })
 
-test('tags are given, including invalid one', () => {
-  expect(findLatestMinor(['v1.0.0', 'v1.x.0', 'v1.2.0'], 1)).toBe(2)
-})
-
-test('tags are given, including different major', () => {
-  expect(findLatestMinor(['v1.0.0', 'v2.3.0', 'v1.2.0'], 1)).toBe(2)
-  expect(findLatestMinor(['v1.0.0', 'v2.3.0', 'v1.2.0'], 2)).toBe(3)
-})
-
-test('only major tag is given', () => {
-  expect(findLatestMinor(['v1'], 1)).toBe(0)
-})
-
-test('only major tags are given', () => {
-  expect(findLatestMinor(['v1', 'v2'], 1)).toBe(0)
+test('invalid tag is given', () => {
+  expect(computeNextTag('v1.x.y', 'v1')).toBe('v1.1.0')
 })
