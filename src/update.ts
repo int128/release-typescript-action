497f49b3e21931be1137ac4e45e06dd4151f40de
Hidetake Iwata <int128@gmail.com>
2025-02-03 10:33:24 +0900

    Do not update major tag on tag event

--- src/update.ts
+++ src/update.ts
@@ -8,8 +8,6 @@ export const followUpCurrentTag = async () => {
   if (!currentTag.startsWith('v')) {
     throw Error(`Tag name should start with v but was ${currentTag}`)
   }
-  const [majorTag] = currentTag.split('.')
-  core.info(`Major tag is ${majorTag}`)
 
   await exec.exec('sed', ['-i', '-E', 's|^/?dist/?||g', '.gitignore'])
   await exec.exec('rm', ['-fr', '.github/workflows'])
@@ -24,8 +22,6 @@ export const followUpCurrentTag = async () => {
   await exec.exec('git', ['config', 'user.email', 'github-actions@github.com'])
   await exec.exec('git', ['commit', '-m', `Release ${currentTag}`])
   await exec.exec('git', ['tag', '-f', currentTag])
-  await exec.exec('git', ['tag', '-f', majorTag])
-  await exec.exec('git', ['push', 'origin', '-f', currentTag, majorTag])
 }
 
 const gitStatus = async (): Promise<string> => {
