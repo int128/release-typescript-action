e71cd2a1e45384e128a8a4ace20154a4f9e85fa2
Hidetake Iwata <int128@gmail.com>
2026-02-01 16:48:39 +0900

    Refactor index.ts

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import assert from 'node:assert'
+import * as core from '@actions/core'
+import { getContext, getOctokit } from './github.js'
+import { run } from './run.js'
+import type { Level } from './semver.js'
+
+const main = async (): Promise<void> => {
+  await run(
+    {
+      majorVersion: Number.parseInt(core.getInput('major-version', { required: true }), 10),
+      incrementLevel: parseLevel(core.getInput('increment-level', { required: true })),
+    },
+    getOctokit(),
+    await getContext(),
+  )
+}
+
+const parseLevel = (s: string): Level => {
+  assert(s === 'minor' || s === 'patch', `increment-level must be either 'minor' or 'patch' but was ${s}`)
+  return s
+}
+
+try {
+  await main()
+} catch (e) {
+  core.setFailed(e instanceof Error ? e : String(e))
+  console.error(e)
+}
